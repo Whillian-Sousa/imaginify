001ead98a9d724bc828632fc5c5d96c4dccece5c
Whillian-Sousas <whillscf@gmail.com>
2024-02-22 02:41:09 -0300

    fix

--- lib/database/models/image.model.ts
+++ lib/database/models/image.model.ts
@@ -1,42 +1,43 @@
-import { Schema, models, model, Document } from "mongoose";
+import { Document, Schema, model, models } from "mongoose";
 
 export interface IImage extends Document {
   title: string;
   transformationType: string;
   publicId: string;
-  secureUrl: string;
+  secureURL: string; 
   width?: number;
   height?: number;
-  config?: object;
-  transformationUrl: string;
+  config?: object; 
+  transformationUrl?: string; 
   aspectRatio?: string;
   color?: string;
   prompt?: string;
   author: {
     _id: string;
     firstName: string;
-  };
-  createAt?: Date;
-  updateAt?: Date;
+    lastName: string;
+  }
+  createdAt?: Date;
+  updatedAt?: Date;
 }
 
 const ImageSchema = new Schema({
   title: { type: String, required: true },
   transformationType: { type: String, required: true },
   publicId: { type: String, required: true },
-  secureUrl: { type: URL, required: true },
+  secureURL: { type: String, required: true },
   width: { type: Number },
   height: { type: Number },
   config: { type: Object },
-  transformationUrl: { type: URL },
+  transformationUrl: { type: String },
   aspectRatio: { type: String },
   color: { type: String },
   prompt: { type: String },
-  author: { type: Schema.Types.ObjectId, ref: "User" },
-  createAt: { type: Date, default: Date.now },
-  updateAt: { type: Date, default: Date.now },
+  author: { type: Schema.Types.ObjectId, ref: 'User' },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
 });
 
-const Image = models?.Image || model("Image", ImageSchema);
+const Image = models?.Image || model('Image', ImageSchema);
 
 export default Image;
